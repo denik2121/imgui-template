5613f9b11acbf858272485ee55111780e69159a5
denik2121 <87583925+denik2121@users.noreply.github.com>
2024-04-14 01:00:05 +0300

    Update main.cpp

--- imgui-template/main.cpp
+++ imgui-template/main.cpp
@@ -45,15 +45,10 @@ void DrawHelloWorldWindow(OpenGLDriver& ogld)
 		static float f = 0.0f;
 		static int counter = 0;
 
-		ImGui::Begin("Hello, world!");								// Create a window called "Hello, world!" and append into it.
-
-		ImGui::Text("This is some useful text.");					// Display some text (you can use a format strings too)
-		ImGui::Checkbox("Demo Window", &show_demo_window);			// Edit bools storing our window open/close state
-		ImGui::Checkbox("Another Window", &show_another_window);
-
-		ImGui::SliderFloat("float", &f, 0.0f, 1.0f);				// Edit 1 float using a slider from 0.0f to 1.0f
-		ImGui::ColorEdit3("clear color", (float*)&ogld.ClearColor); // Edit 3 floats representing a color
-
+		ImGui::GetForegroundDrawList()->AddRect(ImVec2(100, 100), ImVec2(200, 200), IM_COL32(255, 0, 0, 255));
+     	        ImVec2 screenCenter = ImGui::GetIO().DisplaySize * 0.5f;
+                float circleRadius = 50.0f;
+                ImGui::GetForegroundDrawList()->AddCircleFilled(screenCenter, circleRadius, IM_COL32(255, 255, 255, 255), 32);
 		if (ImGui::Button("Button"))								// Buttons return true when clicked (most widgets return true when edited/activated)
 		{
 			counter++;
